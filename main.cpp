1b00081ad8df67114e845efd2728eae241f35fe4
CarmenIJsebaart <cijsebaart@gmail.com>
2018-01-03 20:13:52 +0100

    Did exercise

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
-main(argc, argv) 
-{
+#include <iostream>
 
+int main(int argc, char **argv)
+{
+  if(argc != 1)
+  {
+    std::cout << "Hello" << " " << argv[1] << "\n";
+  }
+  else
+  {
+    std::cout << "Hello\n";
+  }
 }
